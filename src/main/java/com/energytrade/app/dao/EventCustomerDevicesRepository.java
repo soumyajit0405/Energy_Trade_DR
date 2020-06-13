54b76ab787eb8f206dafa5b485928f454cfee23f
Shashvat Gupta <shashvatgupta39@gmail.com>
2020-06-14 03:31:51 +0530

    Updating code for reupload functionality. Code is having but at line 466 in DRDao.java file which is to be further debugged

--- src/main/java/com/energytrade/app/dao/EventCustomerDevicesRepository.java
+++ src/main/java/com/energytrade/app/dao/EventCustomerDevicesRepository.java
@@ -2,8 +2,10 @@
 package com.energytrade.app.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.energytrade.app.model.EventCustomerDevices;
 import com.energytrade.app.model.EventCustomerMapping;
@@ -12,6 +14,9 @@ import com.energytrade.app.model.EventCustomerMapping;
 @Repository
 public interface EventCustomerDevicesRepository extends JpaRepository<EventCustomerDevices, Long> {
 	
-	@Query("Select a from EventCustomerDevices a where a.eventCustomerMapping.eventCustomerMappingId=?1") 
-	EventCustomerDevices getEventCustomerDeviceById(int eventCustomerMappingId);
+	@Modifying
+	@Transactional
+	@Query("delete from EventCustomerDevices where eventCustomerMapping.eventCustomerMappingId in (select eventCustomerMappingId from "
+			+ "EventCustomerMapping where allEvent.eventId in (?1))") 
+	void deleteByEventId(int eventId);
 }
